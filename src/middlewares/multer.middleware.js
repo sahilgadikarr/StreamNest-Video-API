b60b00327a4bb617bef6e519732720b178e8160f
Sahil Gadikar <sahilgadikar@Sahils-MacBook-Air.local>
2026-01-21 17:03:45 +0530

    StreamNest

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname) || ""; // get .mp4, .jpg etc.
+    cb(null, Date.now() + ext);
+  }
+});
+
+export const upload = multer({ 
+    storage,
+})
